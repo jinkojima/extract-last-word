fe8eb35b5c1c2f1b8c0bb1bf8e6f6511173dc8c5
jinkojima <jin.kojima@gmail.com>
2013-07-08 23:13:23 +0900

    to unique extract words

--- extractLastWord.groovy
+++ extractLastWord.groovy
@@ -13,20 +13,26 @@ switch (args.size()) {
 }
 
 //ファイルの各行の最終語句を抽出
+//抽出した語句から重複を除外するように修正
 def extractLastWord(argsMap) {
 
     //対象ファイルと出力先ファイル
     targetFile = new File(argsMap.targetFile)
     outputFile = new File(argsMap.outputFile)
 
+    outputList = []
+
     //各行をsplitして最後の語句を抽出する
     //ただしsize()<=0ならば処理スキップ
     targetFile.eachLine { line->
         if (line.size() > 0) {
             splitedLine = line.split()
-            outputFile.append(splitedLine.last() + "\n")
+            outputList.add(splitedLine.last())
         } else {
             //この行は処理スキップ
         }
+    }
+    outputList.unique().each { item->
+        outputFile.append(item + "\n")
     }
 }
